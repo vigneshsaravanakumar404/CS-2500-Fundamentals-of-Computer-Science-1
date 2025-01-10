2e60f10430590df758698b07c3a4a5b5a841f163
Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>
2025-01-10 10:41:56 -0500

    Finished Lab 1

--- Lab1.rkt
+++ Lab1.rkt
@@ -1,5 +1,6 @@
-#lang racket
-
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Lab1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 
 
@@ -36,4 +37,3 @@
 (above (beside SQR1 SQR2 SQR1)
 (beside SQR2 SQR1 SQR2)
 (beside SQR1 SQR2 SQR1))
-
