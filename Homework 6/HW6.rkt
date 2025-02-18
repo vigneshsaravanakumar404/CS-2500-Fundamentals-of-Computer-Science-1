8435145290590e754aa71dec232eb0d2453e29f9
Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>
2025-02-18 02:54:04 -0500

    Progress HW 6

--- Homework 6/HW6.rkt
+++ Homework 6/HW6.rkt
@@ -1,6 +1,10 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname HW6) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HW6) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+
 ;;                             Exercise 1
 ;; ========================================================================
 (define LIST-1 (cons 1 (cons 2 (cons 3 '()))))
@@ -23,44 +27,47 @@
 
 ; keep-less-than : [List-of Number] Number -> [List-of Number]
 ; Produces a list of numbers less than the given number
-(check-expect (keep-less-than LIST-1 2) (cons 1 '()))
-(check-expect (keep-less-than LIST-2 2) (cons 1 '()))
+(check-expect (keep-less-than LIST-1 2) (cons 1 (cons 2 '())))
+(check-expect (keep-less-than LIST-2 2) (cons 1 (cons 2 '())))
 (check-expect (keep-less-than LIST-3 50) (cons 43 (cons 12 '())))
 
 (define (keep-less-than lon num)
   (cond
     [(empty? lon) '()]
-    [(< (first lon) num) (cons (first lon) (keep-less-than (rest lon) num))]
+    [(<= (first lon) num) (cons (first lon) (keep-less-than (rest lon) num))]
     [else (keep-less-than (rest lon) num)]))
 
 
-; keep-select: [List-of Number] Number ??? -> [List-of Number]
+; keep-select: [List-of Number] Number Number -> [List-of Number]
 ; Produces a list of numbers that satisfy the given predicate
-(check-expect (keep-select LIST-1 2 >=) (cons 2 (cons 3 '())))
-(check-expect (keep-select LIST-2 2 >=) (cons 2 (cons 3 (cons 4 '()))))
-(check-expect (keep-select LIST-3 50 >=) (cons 70 '()))
+(check-expect (keep-select LIST-1 2 -1) (cons 2 (cons 3 '())))
+(check-expect (keep-select LIST-2 2 -1) (cons 2 (cons 3 (cons 4 '()))))
+(check-expect (keep-select LIST-3 50 -1) (cons 70 '()))
+(check-expect (keep-select LIST-1 2 1) (cons 1 (cons 2 '())))
+(check-expect (keep-select LIST-2 2 1) (cons 1 (cons 2 '())))
+(check-expect (keep-select LIST-3 50 1) (cons 43 (cons 12 '())))
 
 (define (keep-select lon num pred)
   (cond
     [(empty? lon) '()]
-    [(pred (first lon) num) (cons (first lon) (keep-select (rest lon) num pred))]
+    [(>= (* pred num) (* pred (first lon))) (cons (first lon) (keep-select (rest lon) num pred))]
     [else (keep-select (rest lon) num pred)]))
 
 
-; New Functions with Abstracted Functions
+; New Functions with Abstracted Functions /FIX/ Did i keep the right parts of the design recipe?
 (check-expect (keep-greater-than/v2 LIST-1 2) (cons 2 (cons 3 '())))
 (check-expect (keep-greater-than/v2 LIST-2 2) (cons 2 (cons 3 (cons 4 '()))))
 (check-expect (keep-greater-than/v2 LIST-3 50) (cons 70 '()))
 
 (define (keep-greater-than/v2 lon num)
-  (keep-select lon num >=))
+  (keep-select lon num -1))
 
-(check-expect (keep-less-than/v2 LIST-1 2) (cons 1 '()))
-(check-expect (keep-less-than/v2 LIST-2 2) (cons 1 '()))
+(check-expect (keep-less-than/v2 LIST-1 2) (cons 1 (cons 2 '())))
+(check-expect (keep-less-than/v2 LIST-2 2) (cons 1 (cons 2 '())))
 (check-expect (keep-less-than/v2 LIST-3 50) (cons 43 (cons 12 '())))
 
 (define (keep-less-than/v2 lon num)
-  (keep-select lon num <))
+  (keep-select lon num 1))
 
 
 ;;                           Exercise 2
@@ -104,11 +111,11 @@
 (check-expect (first-true BOOL-LIST-2) 2)
 (check-expect (first-true BOOL-LIST-3) 0)
 (check-expect (first-true (cons #false '())) -1)
-(check-expect (first-true '(#false #false)) -1)
-(check-expect (first-true '(#false #false #true)) 2)
-(check-expect (first-true '(#false #true #false #true)) 1)
-(check-expect (first-true '(#false #false #false)) -1)
-(check-expect (first-true '(#false #false #false #false #true)) 4)
+(check-expect (first-true (cons #false (cons #false '()))) -1)
+(check-expect (first-true (cons #false (cons #false (cons #true '())))) 2)
+(check-expect (first-true (cons #false (cons #true (cons #false (cons #true '()))))) 1)
+(check-expect (first-true (cons #false (cons #false (cons #false '())))) -1)
+(check-expect (first-true (cons #false (cons #false (cons #false (cons #false (cons #true '())))))) 4)
 
 
 (define (first-true lob)
@@ -123,6 +130,31 @@
     [else (+ 1 (first-true (rest lob)))])) |#
 
 
+;; /FIX/ What does limit unnecessary recursion mean?
+; set-predicate : [List-of Boolean] Number Boolean -> [List-of Boolean]
+; n-th item in the list (counting from 0) set to the given boolean or error if the old value was the same
+(check-expect (set-predicate BOOL-LIST-1 1 #true)
+              (cons #true  (cons #true (cons #true '()))))
+(check-expect (set-predicate BOOL-LIST-2 1 #true)
+              (cons #false (cons #true (cons #true (cons #true '())))))
+(check-expect (set-predicate BOOL-LIST-1 0 #false)
+              (cons #false (cons #false (cons #true '()))))
+(check-expect (set-predicate BOOL-LIST-2 2 #false)
+              (cons #false (cons #false (cons #false (cons #true '())))))
+(check-error (set-predicate BOOL-LIST-1 1 #false) "The old value was not #true")
+(check-error (set-predicate BOOL-LIST-1 2 #true) "The old value was not #false")
+(check-error (set-predicate BOOL-LIST-2 0 #false) "The old value was not #true")
+
+(define (set-predicate lob n pred)
+    (cond
+        [(empty? lob) '()]
+        [(= n 0) (cond
+                   [(or (and (first lob) pred) (and (not (first lob)) (not pred)))
+                    (error (string-append "The old value was not " (boolean->string (not pred))))]
+                    [else (cons pred (rest lob))])]
+        [else (cons (first lob) (set-predicate (rest lob) (- n 1) pred))]))
+
+
 ; set-true : [List-of Boolean] Number -> [List-of Boolean]
 ; n-th item in the list (counting from 0) converted from #false to #true, error if the old value was not #false)
 (check-expect (set-true BOOL-LIST-1 1) (cons #true (cons #true (cons #true '()))))
@@ -131,10 +163,105 @@
 (check-error (set-true BOOL-LIST-2 2) "The old value was not #false")
 
 (define (set-true lob n)
+    (set-predicate lob n #true))
+
+
+; set-false : [List-of Boolean] Number -> [List-of Boolean]
+; n-th item in the list (counting from 0) converted from #true to #false, error if the old value was not #true)
+(check-expect (set-false BOOL-LIST-1 0) (cons #false (cons #false (cons #true '()))))
+(check-expect (set-false BOOL-LIST-2 2) (cons #false (cons #false (cons #false (cons #true '())))))
+(check-error (set-false BOOL-LIST-1 1) "The old value was not #true")
+(check-error (set-false BOOL-LIST-2 0) "The old value was not #true")
+
+(define (set-false lob n)
+    (set-predicate lob n #false))
+
+
+
+; draw-map : [List-of Boolean] Number -> Image
+; Produces an image of a map with the given list of booleans and proportion n:1
+(check-expect (draw-map BOOL-LIST-1 1) (beside (rectangle 20 20 "solid" "black") (beside (rectangle 19 19 "outline" "black") (rectangle 20 20 "solid" "black"))))
+(check-expect (draw-map BOOL-LIST-2 1) (beside (rectangle 19 19 "outline" "black") (beside (rectangle 19 19 "outline" "black") (beside (rectangle 20 20 "solid" "black") (rectangle 20 20 "solid" "black")))))
+(check-expect (draw-map BOOL-LIST-2 4) (beside (rectangle 79 19 "outline" "black") (beside (rectangle 79 19 "outline" "black") (beside (rectangle 80 20 "solid" "black") (rectangle 80 20 "solid" "black")))))
+(check-expect (draw-map BOOL-LIST-3 1) (beside (rectangle 20 20 "solid" "black") (beside (rectangle 20 20 "solid" "black") (rectangle 19 19 "outline" "black"))))
+
+(define (draw-map lob n)
   (cond
-    [(empty? lob) '()]
-    [(= n 0) (if (first lob) (error "The old value was not #false") (cons #true (rest lob)))]
-    [else (cons (first lob) (set-true (rest lob) (- n 1)))]))
+    [(empty? lob) empty-image]
+    [else (beside (rectangle (cond
+                               [(first lob) (* n 20)]
+                               [else (+ -1 (* n 20))])
+                             (cond
+                               [(first lob) 20]
+                               [else 19])
+                             (cond
+                               [(first lob) "solid"]
+                               [else "outline"]) "black")
+                  (draw-map (rest lob) n))]))
+
+;;                           Exercise 3
+;; ========================================================================
+
+;; /FIX/ What does truly exit mean?
+;; /FIX/ Where do big-bang helper functions go?
 
 
-; draw-map : [List-of Boolean] -> Image
+; count-true : [List-of Boolean] -> Number
+; Produces the number of true values in the list
+(check-expect (count-true BOOL-LIST-1) 2)
+(check-expect (count-true BOOL-LIST-2) 2)
+(check-expect (count-true BOOL-LIST-3) 2)
+
+(define (count-true lob)
+  (cond
+    [(empty? lob) 0]
+    [(first lob) (+ 1 (count-true (rest lob)))]
+    [else (count-true (rest lob))]))
+
+; count-total : [List-of Boolean] -> Number
+; Produces the total number of values in the list
+(check-expect (count-total BOOL-LIST-1) 3)
+(check-expect (count-total BOOL-LIST-2) 4)
+(check-expect (count-total BOOL-LIST-3) 3)
+(define (count-total lob)
+  (cond
+    [(empty? lob) 0]
+    [else (+ 1 (count-total (rest lob)))]))
+
+
+; render : World State -> Image
+; Produces an image of the world state with the following properties:
+; - number of true values
+; - visualized as a rectangle
+; - the message "[THIS SPACE FOR RENT]" 
+(define (render ws)
+    (above (text (string-append "True Count: " (number->string (count-true ws))
+                                " Total Count: " (number->string (count-total ws))) 20 "black")
+           (draw-map ws 4)
+           (text "[THIS SPACE FOR RENT]" 20 "black")))
+
+
+; key-expr : World State KeyEvent -> World State
+; Checks if the "q" key is pressed and if so, exits the program
+(define (key-expr ws ke)
+    (cond
+        [(key=? ke "q") ws]
+        [else ws]))
+
+; exit-handler : World State -> Boolean
+; Produces true if the list is empty
+(define (exit-handler ws)
+    (empty? (first ws)))
+
+
+; bit-bucket : [List-of Number] -> World State
+; Produces a world state that is a bit-bucket
+(define (bit-bucket lob)
+  (big-bang lob
+    (to-draw render)
+    (on-key key-expr)))
+
+(bit-bucket BOOL-LIST-2)
+
+; TODO: END PROGRAM
+; TODO: Handle Mouse click to flip
