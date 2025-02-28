d87d775d02e3b86140551c4f04f14e7ab7d579ae
Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>
2025-02-28 01:06:31 -0500

    Finish HW-7

--- Homework 7/HW7-1.rkt
+++ Homework 7/HW7-1.rkt
@@ -2,23 +2,23 @@
 
 ; until-something : (X) [X X -> Boolean] [List of X] -> [List-of X]
 ; Interpretation: To return a list of elements from a list until a certain condition is met.
-(check-expect (until-something = 0 (list 1 2 3 0 4 5)) (list 1 2 3))
-(check-expect (until-something = 0 empty) empty)
-(check-expect (until-something = 0 (list 0)) empty)
-(check-expect (until-something = 0 (list 1 2 3 4 5)) (list 1 2 3 4 5))
-(check-expect (until-something string=? "." (list "Hello" "World" "." "Goodbye"))
+(check-expect (until-something 0 = (list 1 2 3 0 4 5)) (list 1 2 3))
+(check-expect (until-something 0 = empty) empty)
+(check-expect (until-something 0 = (list 0)) empty)
+(check-expect (until-something 0 = (list 1 2 3 4 5)) (list 1 2 3 4 5))
+(check-expect (until-something "." string=? (list "Hello" "World" "." "Goodbye"))
               (list "Hello" "World"))
-(check-expect (until-something string=? "." empty) empty)
-(check-expect (until-something string=? "." (list ".")) empty)
-(check-expect (until-something string=? "." (list "This" "is" "a" "test"))
+(check-expect (until-something "." string=? empty) empty)
+(check-expect (until-something "." string=? (list ".")) empty)
+(check-expect (until-something "." string=? (list "This" "is" "a" "test"))
               (list "This" "is" "a" "test"))
 
-(define (until-something f s loX)
+(define (until-something s f loX)
   (cond
     [(empty? loX) empty]
     [(cons? loX) (if (f (first loX) s)
                      empty
-                     (cons (first loX) (until-something f s (rest loX))))]))
+                     (cons (first loX) (until-something s f (rest loX))))]))
 
 
 
@@ -30,7 +30,7 @@
 (check-expect (until-zero empty) empty)
 
 (define (until-zero lon)
-  (until-something = 0 lon))
+  (until-something 0 = lon))
  
 
 ; words-until-period: [List-of String] -> [List-of String]
@@ -41,5 +41,4 @@
 (check-expect (words-until-period empty) empty)
 
 (define (words-until-period los)
-  (until-something string=? "." los))
-
+  (until-something "." string=? los))
