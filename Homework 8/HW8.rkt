fd52785c47ac4461ef8464566defa84107e294ab
Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>
2025-03-18 17:43:15 -0400

    Completed Exercise 1

--- Homework 8/HW8.rkt
+++ Homework 8/HW8.rkt
@@ -33,12 +33,13 @@
 (define (set-false lob n)
   (cond [(empty? lob) '()]
         [(cons? lob) (if (zero? n)
-                          (cons #false (rest lob))
+                          (cons #f (rest lob))
                           (cons (first lob) (set-false (rest lob) (sub1 n))))]))
 
 ; ====================================================================================
 ; End of Import Functions
 ; ====================================================================================
+
 
 
 ; ====================================================================================
@@ -47,21 +48,21 @@
 
 ; count-total : [List-of Boolean] -> NonNegInteger
 ; Produces the total number of values in the list
-(check-expect (count-total (cons #true (cons #false (cons #true '())))) 3)
-(check-expect (count-total (cons #false (cons #false (cons #true (cons #true '()))))) 4)
-(check-expect (count-total (cons #true (cons #true (cons #false '())))) 3)
+;;; (check-expect (count-total (cons #true (cons #f (cons #true '())))) 3)
+;;; (check-expect (count-total (cons #f (cons #f (cons #true (cons #true '()))))) 4)
+;;; (check-expect (count-total (cons #true (cons #true (cons #f '())))) 3)
 
-(define (count-total lob)
-  (cond
-    [(empty? lob) 0]
-    [else (+ 1 (count-total (rest lob)))]))
+;;; (define (count-total lob)
+;;;   (cond
+;;;     [(empty? lob) 0]
+;;;     [else (+ 1 (count-total (rest lob)))]))
 
 ; ====================================================================================
 ; End of Import Functions
 ; ====================================================================================
 
 
-
+; DATA DEFINITIONS
 (define HBS-1
   (list (list #t #t)
         (list #f #f #f #f)
@@ -106,16 +107,37 @@
    (list #f #f #f #f #f #f #f #f)))
 
 (define HBS-9
-  (list (list #false #false) 
-         (list #false #true #true #false) 
-         (list #false #false #false #false #false #false #false #true) 
-         (list #false #true #true #false #false #false #false #false #false #false #false #false #false #false #false #false)))
-
-
-; Exercise 1a
+  (list (list #f #f) 
+         (list #f #true #true #f) 
+         (list #f #f #f #f #f #f #f #true) 
+         (list #f #true #true #f #f #f #f #f #f #f #f #f #f #f #f #f)))
 
 ; A HierarchicalBitmapSet (HBS) is a [List-of [List-of Boolean]]
 
+(define-struct hbs-alloc [block hbs])
+; A HBSAllocResult is a (make-hbs-alloc Integer HBS)
+; Represents the result of an allocation call, where:
+; - block is the starting block number for the chunk that was allocated;
+;   or -1 if no space available
+; - hbs is the updated HBS after the allocation
+
+(define HBSAR-1 (make-hbs-alloc 2
+                                (list (list #f #t)
+                                      (list #t #f #f #f))))
+(define HBSAR-2 (make-hbs-alloc 0
+                                (list (list #t #t)
+                                      (list #f #f #f #f))))
+(define HBSAR-3 (make-hbs-alloc -1
+                                (list (list #t #t)
+                                      (list #f #f #f #f))))
+
+(define (hbsar-temp hbsar)
+  (... (hbs-alloc-block hbsar) ...
+       (hbs-temp (hbs-alloc-hbs hbsar) ...)))
+
+
+
+; Exercise 1a
 ; blocks-remaining : HBS -> NonNegInteger
 ; Produces the number of blocks remaining in the list of blocks
 (check-expect (blocks-remaining HBS-1) 8)
@@ -154,19 +176,29 @@
       [else (find-chunk (rest HBS) s)])))
     
 
-;! FINISH DESIGN RECIPES FOR EXERCISE 1c
 ; Exercise 1c
 ; initialize-hbs : [List-of Boolean] -> HBS
 ; consumes a single bitmap of the free blocks on a drive, and produces the corresponding HBS 
-(check-expect (initialize-hbs (last HBS-7)) HBS-7)
+(check-expect (initialize-hbs (list #t #t #t #t #t #t #t #t)) HBS-1)
+(check-expect (initialize-hbs (list #t #t #t #t #t #t #f #f)) HBS-2)
+(check-expect (initialize-hbs (list #f #f #f #f #f #f #f #f)) HBS-5)
+(check-expect (initialize-hbs (list #t #t #t #t #t #t #t #f)) HBS-6)
+(check-expect (initialize-hbs (list #t #t #t #t #t #t #t #t)) HBS-8)
 
 (define (initialize-hbs lob)
-  (initialize-hbs-helper lob '()))
+  (local [(define (r lob o)
+            (cond
+              [(= (length lob) 2) (cons lob o)]
+              [else (r (pairwise-and lob) (cons (coalesce lob) o))]))]
+    (r lob '())))
 
-(define (initialize-hbs-helper lob o)
-  (cond
-    [(= (length lob) 2) (cons lob o)]
-    [else (initialize-hbs-helper (pairwise-and lob) (cons (coalesce lob) o))]))
+; pairwise-and : [List-of Boolean] -> [List-of Boolean]
+; consumes a list of booleans and produces a list of booleans 1/2 the size of the input list 
+; representing the level above
+(check-expect (pairwise-and (list #t #t #f #f)) (list #t #f))
+(check-expect (pairwise-and (list #t #t #t #t)) (list #t #t))
+(check-expect (pairwise-and (list #f #f #f #f)) (list #f #f))
+(check-expect (pairwise-and (list #t #f #t #f)) (list #f #f))
 
 (define (pairwise-and lst)
   (cond
@@ -176,36 +208,21 @@
 
 ; coalesce : [List-of Boolean] -> [List-of Boolean]
 ; consumes a list of booleans and produces a list of booleans with every pair of booleans coalesced
+; where adjacent trues are replaced with false
+(check-expect (coalesce (list #t #t #f #f)) (list #f #f #f #f))
+(check-expect (coalesce (list #t #t #t #t)) (list #f #f #f #f))
+(check-expect (coalesce (list #f #f #f #f)) (list #f #f #f #f))
+(check-expect (coalesce (list #t #f #t #f)) (list #t #f #t #f))
+(check-expect (coalesce (list #t #f #t #t)) (list #t #f #f #f))
+
 (define (coalesce  lst)
     (cond
       [(empty? lst) '()]
-      [(and (first lst) (second lst)) (cons #false (cons #false (coalesce (rest (rest lst)))))]
+      [(and (first lst) (second lst)) (cons #f (cons #f (coalesce (rest (rest lst)))))]
       [else (cons (first lst) (cons (second lst) (coalesce  (rest (rest lst)))))]))
 
 
 ; Exercise 2
-(define-struct hbs-alloc [block hbs])
-; A HBSAllocResult is a (make-hbs-alloc Integer HBS)
-; Represents the result of an allocation call, where:
-; - block is the starting block number for the chunk that was allocated;
-;   or -1 if no space available
-; - hbs is the updated HBS after the allocation
-
-(define HBSAR-1 (make-hbs-alloc 2
-                                (list (list #f #t)
-                                      (list #t #f #f #f))))
-(define HBSAR-2 (make-hbs-alloc 0
-                                (list (list #t #t)
-                                      (list #f #f #f #f))))
-(define HBSAR-3 (make-hbs-alloc -1
-                                (list (list #t #t)
-                                      (list #f #f #f #f))))
-
-(define (hbsar-temp hbsar)
-  (... (hbs-alloc-block hbsar) ...
-       (hbs-temp (hbs-alloc-hbs hbsar) ...)))
-
-
 ; alloc-chunk : HBS NonNegInteger -> HBS
 ; consumes a HBS and a size s, and produces a new HBS with the first free chunk of size s allocated
 ;;; (check-expect (alloc-chunk (list (list #f #t) (list #f #t #f #f) (list #f #t #f #f #f #f #f #f)) 2)
